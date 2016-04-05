5da3737639b275d358ac9da317fab6b4ff878b09
oscar <fc_oscar@hotmail.com>
2016-04-05 16:51:57 +0200

    add to ambrosio.py chanels

--- ambrosio/ambrosio.py
+++ ambrosio/ambrosio.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from commandlist import CommandList
+import channels as ch
 import time
 
 class Ambrosio(object):
@@ -11,12 +12,19 @@ class Ambrosio(object):
     def __init__(self):
         super(Ambrosio, self).__init__()
         self.c1 = CommandList()
+        self.channels = []
+        self.channels.append(ch.TextChannel())
 
     def next_command(self):
         try:
             return self.c1.next()
         except:
             return None
+
+    def update_channels(self):
+        for chan in self.channels:
+            while chan.msg_avail():
+                self.c1.append(chan.get_msg())
 
 
     def mainloop(self):
@@ -27,6 +35,7 @@ class Ambrosio(object):
         while True:
             command = self.next_command()
             time.sleep(1)
+            self.update_channels()
 
 
 if __name__ == "__main__":
